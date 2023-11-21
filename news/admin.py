2a5048d0a4e8d3d0e2ba80133335e51e848d3929
Ngozi89 <>
2023-11-21 20:17:22 +0000

    Create post model, post admin, filter post box, list display, search filed and approve comment

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+# Register post models and post admin class.
+@admin.register(Post)
+# Post admin class
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+# Search field.
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+# Search through comments.
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+# Approve comment
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Pulished"))
+
+
+# Create post models.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='newspost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+# Add string that returns representation of an object.
+    def __str__(self):
+        return self.title
+
+# Returns total number of likes on a post.
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+# Add comment models.
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
