63d2325f858746455a8324a2bc883e613540564c
Ngozi89 <>
2023-11-30 19:26:30 +0000

    Add copyright

--- news/urls.py
+++ news/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('about', views.about, name="about"),
     path('policy', views.policy, name="policy"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
