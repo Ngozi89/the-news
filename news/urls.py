fbc12599f997c5c2a1285c6f62527c5f18f2fefe
Ngozi Omenka <ngozi.madu@live.se>
2024-06-10 12:16:55 +0000

    Added add article template and views

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('addarticles/', views.AddArticle.as_view(), name='add_article'),
     path('about', views.about, name="about"),
     path('policy', views.policy, name="policy"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
--- news/views.py
+++ news/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from .models import Post, Comment, Reply, Profile
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
-from .forms import CommentForm, ProfileForm
+from .forms import CommentForm, ProfileForm, ArticleForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 
@@ -80,6 +80,34 @@ class Profile(LoginRequiredMixin, generic.TemplateView):
     
     def __str__(self):
         return f"Profile {self.user}by{self.bio}"
+
+
+class AddArticle(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
+    """
+    This view is used to allow logged in users to create a post
+    """
+    form_class = ArticleForm
+    template_name = 'add_article.html'
+    success_message = "%(calculated_field)s was created successfully"
+
+    def form_valid(self, form):
+        """
+        This method is called when valid form data has been posted.
+        The signed in user is set as the author of a posted article.
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_message(self, cleaned_data):
+        """
+        This function overrides the get_success_message method to add
+        the article title into the success message.
+        """
+        return self.success_message % dict(
+            cleaned_data,
+            calculated_field=self.object.title,
+        )
+
    
 
 class ReplyComment(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, generic.View):
